0f51ece1b5b44065c9009487eeba448a1d92cf44
IndalDA_SCS <148204353+IndalDA@users.noreply.github.com>
2025-08-22 20:09:38 +0530

    Update hrpt.py

--- hrpt.py
+++ hrpt.py
@@ -117,9 +117,10 @@ def process_files(validation_errors, all_locations, start_date, end_date, total_
                     sd['New partcat'] = None
 
                 # category filter
-                if select_categories:
+                if select_categories!='All':
                     sel = set([str(x).strip().lower() for x in select_categories])
                     sd = sd[sd['New partcat'].astype(str).str.lower().isin(sel)]
+                  
 
                 out = sd[['Brand', 'Dealer', 'Location', part_col, qty_col]].copy()
                 out.rename(columns={part_col: 'Partnumber', qty_col: 'Qty'}, inplace=True)
@@ -521,6 +522,7 @@ def process_files(validation_errors, all_locations, start_date, end_date, total_
     else:
         st.info("ℹ No reports available to download.")
         st.warning("Pls check Folder Structure")  # (fix typo from st.warring -> st.warning)
+
 
 
 
