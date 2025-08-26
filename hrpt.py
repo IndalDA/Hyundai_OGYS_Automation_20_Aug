872098849dcc239985d7c14a44f765853174a6d5
IndalDA_SCS <148204353+IndalDA@users.noreply.github.com>
2025-08-26 16:19:01 +0530

    Update hrpt.py

--- hrpt.py
+++ hrpt.py
@@ -75,7 +75,8 @@ def process_files(validation_errors, all_locations, start_date, end_date, total_
                 try:
                   bo_df.columns = custom_headers[:bo_df.shape[1]]
                 except:
-                  bo_df = pd.read_html(file_path,header=1)
+                  bo_df = pd.concat(pd.read_html(file_path,header=1),ignore_index=True)
+                  
                   bo_df.columns = custom_headers[:bo_df.shape[1]]
                 if bo_df is None or bo_df.empty:
                     validation_errors.append(f"{location}: Unable to read BO LIST -> {file}")
@@ -554,6 +555,7 @@ def process_files(validation_errors, all_locations, start_date, end_date, total_
     else:
         st.info("ℹ No reports available to download.")
         st.warning("Pls check Folder Structure")  # (fix typo from st.warring -> st.warning)
+
 
 
 
